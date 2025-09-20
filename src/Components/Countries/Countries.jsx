83f12a778cb086ce1723de249371b185e5a2d128
Mohiuddin5050 <kmmohiuddin5050@gmail.com>
2025-09-20 22:30:42 +0600

    react project

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -3,24 +3,24 @@ import Country from '../Country/Country';
 import './Countries.css'
 
 const Countries = ({ countriesPromise }) => {
-    const [visitedCountries, setVisitedCountries]=useState([]);
-    const handleVisitedCountries=(country)=>{
-        const newVisitedCountries=[...visitedCountries,country]
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const handleVisitedCountries = (country) => {
+        const newVisitedCountries = [...visitedCountries, country]
         setVisitedCountries(newVisitedCountries)
     }
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
     return (
         <div>
-            <h1>In the countries: {countries.length}</h1>
+            <h1>Visited countries: {countries.length}</h1>
             <h3>Total Country Visited: {visitedCountries.length}</h3>
             <div className='countries'>
                 {
-                countries.map(country=><Country
-                    key={country.ccn3.ccn3}
-                     country={country}
-                     handleVisitedCountries={handleVisitedCountries}></Country>)
-            }
+                    countries.map(country => <Country
+                        key={country.ccn3.ccn3}
+                        country={country}
+                        handleVisitedCountries={handleVisitedCountries}></Country>)
+                }
             </div>
         </div>
     );
